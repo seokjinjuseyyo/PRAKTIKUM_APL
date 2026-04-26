ba132b05f8a14fbfb8985bfa9eb26d76ab82f6bc
seokjinjuseyyo <seokjinjuseyyo1@gmail.com>
2026-04-26 17:50:49 +0700

    Finish Post Test 7

---
+++ Kelas/pertemuan 7/pertemuan7.cpp
@@ -0,0 +1,16 @@
+/*#include <iostream>
+using namespace std;
+int main() {
+try {
+int a = 10;
+int b = 0;
+if (b = 0) {
+throw runtime_error("Error pembagian dengan 0");
+}
+int c = a / b;
+cout << "Hasil: " << c << endl;
+} catch (const runtime_error& e) {
+cout << "Error ditangkap: " << e.what() << endl;
+}
+return 0;
+}*/
